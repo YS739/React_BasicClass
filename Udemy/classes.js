001a6d16d14cbca219ae266642dd722270bbc169
YS739 <suk921@gmail.com>
2022-12-12 09:55:13 +0900

    changed all file

--- Udemy/classes.js
+++
@@ -1,8 +0,0 @@
-class Person {
-  constructor() {
-    this.name = "Yunny";
-  }
-}
-
-const person = new Person();
-console.log(person.name);
--- Udemy/lestSpread.js
+++
@@ -1,4 +0,0 @@
-const filter = (...args) => {
-  return args.filter((e) => e === 2);
-};
-console.log(filter(1, 2, 3));
