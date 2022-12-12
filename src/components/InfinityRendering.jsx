a2f3cd68c26d5993953573b2b413807274125763
YS739 <suk921@gmail.com>
2022-12-12 11:08:50 +0900

    dependency array start

--- src/components/InfinityRendering.jsx
+++ src/components/InfinityRendering.jsx
@@ -5,7 +5,8 @@ const InfinityRendering = () => {
 
   useEffect(() => {
     console.log("hello useEffect");
-  });
+  }, []);
+  //  component가 렌더링 될 때 한 번만 사용하고 싶으면 의존성 배열을 '[]' 빈 상태로 두기
 
   return (
     <div>
