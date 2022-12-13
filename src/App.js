9d32d761884f3d24c6ba2164b9c2ba30f084dda9
YS739 <suk921@gmail.com>
2022-12-13 11:22:42 +0900

    router setting done - connect app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 // src/App.js
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addNumber, minusNumber } from "./redux/modules/counter";
+import Router from "./shared/Router";
 
 const App = () => {
-  const [number, setNumber] = useState(0);
-  const globalNumber = useSelector((state) => state.counter.number);
-
-  const dispatch = useDispatch();
-
-  const onClickAddNumberHandler = () => {
-    dispatch(addNumber(number));
-  };
-
-  const onClickMinusNumberHandler = () => {
-    dispatch(minusNumber(number));
-  };
-
-  const onChangeHandler = (event) => {
-    const { value } = event.target;
-    setNumber(+value);
-  };
-  console.log(number);
-
-  return (
-    <div>
-      {globalNumber}
-      <input type="number" onChange={onChangeHandler} />
-      <button onClick={onClickAddNumberHandler}>더하기</button>
-      <button onClick={onClickMinusNumberHandler}>빼기</button>
-    </div>
-  );
+  return <Router />;
 };
 
 export default App;
