7a4725ddf7495fca4ccf9523a286b5e849cb4ccb
YS739 <suk921@gmail.com>
2022-12-13 11:33:10 +0900

    use Link at works.js

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,12 +1,14 @@
 // src/pages/works.js
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Works = () => {
   const location = useLocation();
   console.log("location :>> ", location);
   return (
     <div>
-      <button>버튼</button>
+      {`현재 페이지: ${location.pathname.slice(1)}`}
+      <br />
+      <Link to="/contact">contact 페이지로 이동</Link>
     </div>
   );
 };
