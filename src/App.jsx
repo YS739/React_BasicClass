047cbaa30bc54127c339e6c4ed45b383a2e96e3e
YS739 <suk921@gmail.com>
2022-12-19 12:22:01 +0900

    Add Axois delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 // src/App.jsx
 
 import React, { useEffect, useState } from "react";
-import axios from "axios"; // axios import 합니다.
+import axios from "axios";
 
 const App = () => {
-  // 새롭게 생성하는 todo를 관리하는 state
   const [todo, setTodo] = useState({
     title: "",
   });
@@ -16,26 +15,23 @@ const App = () => {
     setTodos(data);
   };
 
-  const onSubmitHandler = async (todo) => {
-    //1.  이때 todos는 [{투두하나}]임
-    await axios.post("http://localhost:3001/todos", todo); // 이때 서버에 있는 todos도 [{투두하나}]임
+  const onSubmitHandler = (todo) => {
+    axios.post("http://localhost:3001/todos", todo);
+  };
 
-    // 근데 여기서 서버 요청이 끝나고 서버는 [{투두가},{두개임}]
-
-    setTodos([...todos, todo]); //2. <-- 만약 이게 없다면, go to useEffect
-    //4. 새로고침해서 진짜 현재 서버 데이터를 받아오기전에 상태를 똑같이 동기시켜줌
-    //5. 어떻게보면 유저한테 서버에서 새로 받아온것처럼 속이는거지
+  // 새롭게 추가한 삭제 버튼 이벤트 핸들러
+  const onClickDeleteButtonHandler = (todoId) => {
+    axios.delete(`http://localhost:3001/todos/${todoId}`);
   };
 
   useEffect(() => {
-    fetchTodos(); //3. 새로고침해서 여기를 다시 실행해줘야 서버값이 새로 들어옴 e.g) [{투두가},{두개임}]
+    fetchTodos();
   }, []);
 
   return (
     <>
       <form
         onSubmit={(e) => {
-          // 👇 submit했을 때 브라우저의 새로고침을 방지합니다.
           e.preventDefault();
           onSubmitHandler(todo);
         }}
@@ -54,7 +50,16 @@ const App = () => {
       </form>
       <div>
         {todos?.map((todo) => (
-          <div key={todo.id}>{todo.title}</div>
+          <div key={todo.id}>
+            {todo.title}
+            {/*  디자인이 요상하긴 하지만..! 삭제 버튼 추가 */}
+            <button
+              type="button"
+              onClick={() => onClickDeleteButtonHandler(todo.id)}
+            >
+              삭제하기
+            </button>
+          </div>
         ))}
       </div>
     </>
